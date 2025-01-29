3e9c016157f42ad749ab91addf907004b6a24b24
Marek Wannepain <marekwannepain@gmail.com>
2025-01-29 09:14:01 +0100

    Implement dynamic content for onboarding template page based on type

--- lib/pages/onboarding/sub_pages/template_page.dart
+++ lib/pages/onboarding/sub_pages/template_page.dart
@@ -10,6 +10,88 @@ class TemplatePage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    Widget? paragraph;
+    String title = '';
+    String header = '';
+    String illustration = '';
+    String buttonText = '';
+
+    switch (type.toLowerCase()) {
+      case 'purpose':
+        title = 'Purpose';
+        header = 'Find Your';
+        illustration = 'assets/illustrations/purpose_illustration.svg';
+        buttonText = 'Try';
+        paragraph = RichText(
+          textAlign: TextAlign.center,
+          text: TextSpan(
+            style: TextStyle(
+              fontSize: 24,
+              color: Colors.black,
+              fontFamily: GoogleFonts.inter().fontFamily,
+            ),
+            children: <TextSpan>[
+              TextSpan(text: 'Discover your '),
+              TextSpan(
+                  text: 'Purpose ',
+                  style: const TextStyle(fontWeight: FontWeight.bold)),
+              TextSpan(text: 'through '),
+              TextSpan(
+                  text: 'meaningful dialogue',
+                  style: const TextStyle(fontWeight: FontWeight.bold)),
+            ],
+          ),
+        );
+        break;
+      case 'clarity':
+        title = 'Clarity';
+        header = 'Decide with';
+        illustration = 'assets/illustrations/clarity_illustration.svg';
+        buttonText = 'Ask';
+        paragraph = RichText(
+          textAlign: TextAlign.center,
+          text: TextSpan(
+            style: TextStyle(
+              fontSize: 24,
+              color: Colors.black,
+              fontFamily: GoogleFonts.inter().fontFamily,
+            ),
+            children: <TextSpan>[
+              TextSpan(text: 'Unsure about your '),
+              TextSpan(
+                  text: 'Future ',
+                  style: const TextStyle(fontWeight: FontWeight.bold)),
+              TextSpan(text: '?'),
+            ],
+          ),
+        );
+        break;
+      case 'confidence':
+        title = 'Confident';
+        header = 'Be';
+        illustration = 'assets/illustrations/confident_illustration.svg';
+        buttonText = 'Try';
+        paragraph = RichText(
+          textAlign: TextAlign.center,
+          text: TextSpan(
+            style: TextStyle(
+              fontSize: 24,
+              color: Colors.black,
+              fontFamily: GoogleFonts.inter().fontFamily,
+            ),
+            children: <TextSpan>[
+              TextSpan(text: 'Adwis gives you all the '),
+              TextSpan(
+                  text: 'information ',
+                  style: const TextStyle(fontWeight: FontWeight.bold)),
+              TextSpan(text: 'you need to decide. '),
+            ],
+          ),
+        );
+        break;
+      default:
+    }
+
     return Scaffold(
       body: Column(
         mainAxisSize: MainAxisSize.max,
@@ -21,7 +103,7 @@ class TemplatePage extends StatelessWidget {
               crossAxisAlignment: CrossAxisAlignment.stretch,
               children: [
                 Text(
-                  'Find Your',
+                  header,
                   textAlign: TextAlign.center,
                   style: TextStyle(
                     fontSize: 20,
@@ -30,7 +112,7 @@ class TemplatePage extends StatelessWidget {
                 ),
                 SizedBox(height: 30),
                 Text(
-                  'Purpose'.toUpperCase(),
+                  title.toUpperCase(),
                   textAlign: TextAlign.center,
                   style: TextStyle(
                       fontSize: 48,
@@ -39,50 +121,21 @@ class TemplatePage extends StatelessWidget {
                 ),
                 SizedBox(height: 30),
                 SvgPicture.asset(
-                  "assets/illustrations/purpose_illustration.svg",
+                  illustration,
                   width: 300,
                   height: 300,
                   fit: BoxFit.contain,
                 ),
                 SizedBox(height: 30),
-                // Text(
-                //   "Discover your purpose through meaningful dialogue",
-                //   textAlign: TextAlign.center,
-                //   style: TextStyle(
-                //     fontSize: 24,
-                //     fontFamily: GoogleFonts.inter().fontFamily,
-                //   ),
-                // )
-                RichText(
-                  text: TextSpan(
-                    // Note: Styles for TextSpans must be explicitly defined.
-                    // Child text spans will inherit styles from parent
-                    style: TextStyle(
-                      fontSize: 24,
-                      color: Colors.black,
-                      fontFamily: GoogleFonts.inter().fontFamily,
-                    ),
-
-                    children: <TextSpan>[
-                      TextSpan(text: 'Discover your '),
-                      TextSpan(
-                          text: 'Purpose ',
-                          style: const TextStyle(fontWeight: FontWeight.bold)),
-                      TextSpan(text: 'through '),
-                      TextSpan(
-                          text: 'meaningful dialogue',
-                          style: const TextStyle(fontWeight: FontWeight.bold)),
-                    ],
-                  ),
-                )
+                paragraph ?? Text('Failed to load paragraph'),
               ],
             ),
           ),
           ActionButton(
             onPressed: () {
-              // Navigator.pushNamed(context, '/onboarding/step1');
+              print('Try button pressed');
             },
-            text: 'Try',
+            text: buttonText,
           ),
         ],
       ),
