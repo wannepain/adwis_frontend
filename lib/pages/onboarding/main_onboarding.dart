1cc2b3a4cfff907180f0c417de0d78414c5bb26b
Marek Wannepain <marekwannepain@gmail.com>
2025-01-29 09:14:10 +0100

    Add main onboarding page with automatic page rotation

---
+++ lib/pages/onboarding/main_onboarding.dart
@@ -0,0 +1,52 @@
+import 'package:flutter/material.dart';
+import 'package:adwis_frontend/pages/onboarding/sub_pages/template_page.dart';
+import 'dart:async';
+
+class MainOnboarding extends StatefulWidget {
+  const MainOnboarding({super.key});
+
+  @override
+  _MainOnboardingState createState() => _MainOnboardingState();
+}
+
+class _MainOnboardingState extends State<MainOnboarding> {
+  var currentPage = 1;
+  Timer? _timer;
+
+  @override
+  void initState() {
+    super.initState();
+    _startTimer();
+  }
+
+  void _startTimer() {
+    _timer = Timer.periodic(Duration(seconds: 5), (timer) {
+      setState(() {
+        if (currentPage == 3) {
+          currentPage = 1;
+        } else {
+          currentPage++;
+        }
+      });
+    });
+  }
+
+  @override
+  void dispose() {
+    _timer?.cancel();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    print('Current Page: $currentPage'); // Debug print
+
+    if (currentPage == 1) {
+      return TemplatePage(type: "purpose");
+    } else if (currentPage == 2) {
+      return TemplatePage(type: "clarity");
+    } else {
+      return TemplatePage(type: "confidence");
+    }
+  }
+}
