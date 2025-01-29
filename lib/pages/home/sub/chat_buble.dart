3e159a3301ac38603e3309f7c2d5b3412c424f27
Marek Wannepain <marekwannepain@gmail.com>
2025-01-29 09:41:01 +0100

    Add ChatBuble widget for chat message display

---
+++ lib/pages/home/sub/chat_buble.dart
@@ -0,0 +1,42 @@
+import 'package:flutter/material.dart';
+
+class ChatBuble extends StatelessWidget {
+  const ChatBuble({super.key, required this.text, required this.isMe});
+
+  final String text;
+  final bool isMe;
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      mainAxisAlignment: isMe ? MainAxisAlignment.end : MainAxisAlignment.start,
+      mainAxisSize: MainAxisSize.max,
+      children: [
+        Container(
+          padding: EdgeInsets.symmetric(horizontal: 10, vertical: 6),
+          margin: EdgeInsets.symmetric(vertical: 10),
+          decoration: BoxDecoration(
+            borderRadius: BorderRadius.all(Radius.circular(9)),
+            color: isMe
+                ? Color.fromRGBO(143, 198, 238, 1)
+                : Color.fromRGBO(237, 243, 248, 1),
+            boxShadow: [
+              BoxShadow(
+                color: isMe
+                    ? Color.fromRGBO(143, 198, 238, 0.5)
+                    : Color.fromRGBO(237, 243, 248, 0.5),
+                spreadRadius: 0,
+                blurRadius: 4,
+                offset: Offset(0, 0),
+              ),
+            ],
+          ),
+          child: Text(
+            text,
+            style: TextStyle(fontSize: 16),
+          ),
+        )
+      ],
+    );
+  }
+}
