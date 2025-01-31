e08e16d92238807dc72388edb2a446b3187248dd
Marek Wannepain <marekwannepain@gmail.com>
2025-01-31 14:24:04 +0100

    Add HTTP request functionality in MainApp for testing purposes

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
+//import 'package:adwis_frontend/pages/home/homepage.dart';
 import 'package:adwis_frontend/pages/home/homepage.dart';
 import 'package:flutter/material.dart';
+import 'package:dio/dio.dart';
 // import 'package:adwis_frontend/pages/onboarding/main_onboarding.dart';
 
 void main() {
@@ -7,14 +9,27 @@ void main() {
 }
 
 class MainApp extends StatelessWidget {
-  const MainApp({super.key});
+  MainApp({super.key});
+  final dio = Dio();
+
+  void testHttp() async {
+    try {
+      final response = await dio.get("https://b875-45-84-122-8.ngrok-free.app");
+      print(response);
+    } catch (e) {
+      print(e);
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Adwis',
       home: Scaffold(
-        body: Container(padding: EdgeInsets.all(6), child: Homepage()),
+        body: Container(
+          padding: EdgeInsets.all(6),
+          child: Homepage(),
+        ),
       ),
     );
   }
