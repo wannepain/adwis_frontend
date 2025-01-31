ec0f58de4c0fb6dfd2ad0ebab1df8c2cf3c84ae6
Marek Wannepain <marekwannepain@gmail.com>
2025-01-31 14:23:57 +0100

    Refactor Homepage to integrate HTTP communication and display chat history

--- lib/pages/home/homepage.dart
+++ lib/pages/home/homepage.dart
@@ -1,7 +1,10 @@
-//import 'package:adwis_frontend/pages/home/sub/chat_buble.dart';
-import 'package:adwis_frontend/pages/home/sub/text_input.dart';
-import 'package:flutter/material.dart';
+import 'dart:convert';
 import 'package:dio/dio.dart';
+import 'package:flutter/material.dart';
+// import 'package:dio/dio.dart';
+import 'package:adwis_frontend/pages/home/sub/chat_buble.dart';
+import 'package:adwis_frontend/pages/home/sub/text_input.dart';
+import 'package:http/http.dart' as http;
 
 class Homepage extends StatefulWidget {
   const Homepage({super.key});
@@ -13,12 +16,30 @@ class Homepage extends StatefulWidget {
 class _HomepageState extends State<Homepage> {
   final dio = Dio();
 
-  void getHttp() async {
+  List history = [];
+  List usedQuestionIdx = [];
+
+  void returnText(String text) {
+    if (history.isEmpty) {
+      return;
+    }
+    setState(() {
+      history[history.length - 1]['client'] = text;
+    });
+    //postHttp();
+  }
+
+  void postHttp() async {
     try {
-      final response = await dio.post('http://10.0.5.80:5000/response', data: {
-        'history': [],
+      final response = await dio.post(
+          "https://b875-45-84-122-8.ngrok-free.app/respond",
+          data: {"history": history, "used_question_idx": usedQuestionIdx});
+
+      setState(() {
+        history = jsonDecode(response.data)['history'];
+        usedQuestionIdx = jsonDecode(response.data)['used_question_idx'];
       });
-      print(response);
+      print(jsonDecode(response.data));
     } catch (e) {
       print('Error: $e');
     }
@@ -26,9 +47,42 @@ class _HomepageState extends State<Homepage> {
 
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: ElevatedButton(
-          onPressed: getHttp, child: Text("test the connection")),
+    postHttp();
+    return Scaffold(
+      body: Column(
+        children: [
+          Expanded(
+            child: Column(
+                children: history.isEmpty
+                    ? [ChatBuble(text: "No history", isMe: true)]
+                    : history.map<Widget>((e) {
+                        String? bot = e['bot']['Question_Text'];
+                        String? client = e['client'];
+                        List<Widget> toReturn = [];
+                        if (bot != null) {
+                          toReturn.add(
+                            ChatBuble(
+                              text: bot ?? "",
+                              isMe: false,
+                            ),
+                          );
+                        }
+                        if (client != null) {
+                          toReturn.add(
+                            ChatBuble(
+                              text: client ?? "",
+                              isMe: true,
+                            ),
+                          );
+                        }
+                        return Column(
+                            children:
+                                toReturn.isEmpty ? [Text("error")] : toReturn);
+                      }).toList()),
+          ),
+          TextInput(returnText: returnText)
+        ],
+      ),
     );
   }
 }
