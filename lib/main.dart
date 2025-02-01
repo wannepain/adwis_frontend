ef226340db81808ce940a453f98670a277a2f78a
Marek Wannepain <marekwannepain@gmail.com>
2025-02-01 16:17:33 +0100

    Replace Homepage with MainOnboarding in MainApp for onboarding flow

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 //import 'package:adwis_frontend/pages/home/homepage.dart';
 import 'package:adwis_frontend/pages/home/homepage.dart';
+import 'package:adwis_frontend/pages/onboarding/main_onboarding.dart';
 import 'package:flutter/material.dart';
 import 'package:dio/dio.dart';
 // import 'package:adwis_frontend/pages/onboarding/main_onboarding.dart';
@@ -29,7 +30,7 @@ class MainApp extends StatelessWidget {
       home: Scaffold(
         body: Container(
           padding: EdgeInsets.all(6),
-          child: Homepage(),
+          child: MainOnboarding(),
         ),
       ),
     );
