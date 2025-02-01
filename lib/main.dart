43b34966abbb01989099297b0d6f683110d7867e
Marek Wannepain <marekwannepain@gmail.com>
2025-02-01 16:17:47 +0100

    Remove commented import for Homepage in main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,6 @@
-//import 'package:adwis_frontend/pages/home/homepage.dart';
-import 'package:adwis_frontend/pages/home/homepage.dart';
 import 'package:adwis_frontend/pages/onboarding/main_onboarding.dart';
 import 'package:flutter/material.dart';
 import 'package:dio/dio.dart';
-// import 'package:adwis_frontend/pages/onboarding/main_onboarding.dart';
 
 void main() {
   runApp(MainApp());
