cb79229b44915757057c6277cc6d50cf50ef9751
Marek Wannepain <marekwannepain@gmail.com>
2025-02-01 10:38:58 +0100

    Refactor Homepage to improve HTTP data handling and enhance chat history display

--- lib/pages/home/homepage.dart
+++ lib/pages/home/homepage.dart
@@ -1,10 +1,9 @@
 import 'dart:convert';
-import 'package:dio/dio.dart';
 import 'package:flutter/material.dart';
-// import 'package:dio/dio.dart';
+import 'package:dio/dio.dart';
 import 'package:adwis_frontend/pages/home/sub/chat_buble.dart';
 import 'package:adwis_frontend/pages/home/sub/text_input.dart';
-import 'package:http/http.dart' as http;
+// import 'package:http/http.dart' as http;
 
 class Homepage extends StatefulWidget {
   const Homepage({super.key});
@@ -26,62 +25,80 @@ class _HomepageState extends State<Homepage> {
     setState(() {
       history[history.length - 1]['client'] = text;
     });
-    //postHttp();
+    setData();
   }
 
-  void postHttp() async {
+  void setData() async {
+    Map result = await postHttp();
+    print(result);
+    setState(() {
+      history = result["history"] ?? [];
+      usedQuestionIdx = result["usedQuestionIdx"] ?? [];
+    });
+  }
+
+  Future<Map> postHttp() async {
     try {
+      print("history: $history");
+      print("used idxs: $usedQuestionIdx");
       final response = await dio.post(
-          "https://b875-45-84-122-8.ngrok-free.app/respond",
+          "https://8e26-45-84-122-28.ngrok-free.app/respond",
           data: {"history": history, "used_question_idx": usedQuestionIdx});
 
-      setState(() {
-        history = jsonDecode(response.data)['history'];
-        usedQuestionIdx = jsonDecode(response.data)['used_question_idx'];
-      });
-      print(jsonDecode(response.data));
+      return {
+        "history": jsonDecode(response.data)['history'],
+        "usedQuestionIdx": jsonDecode(response.data)['used_question_idx'],
+        "error": false,
+      };
     } catch (e) {
       print('Error: $e');
+      return {"history": null, "usedQuestionIdx": null, "error": e};
     }
   }
 
   @override
+  void initState() {
+    setData();
+    super.initState();
+  }
+
+  @override
   Widget build(BuildContext context) {
-    postHttp();
     return Scaffold(
-      body: Column(
-        children: [
-          Expanded(
-            child: Column(
-                children: history.isEmpty
-                    ? [ChatBuble(text: "No history", isMe: true)]
-                    : history.map<Widget>((e) {
-                        String? bot = e['bot']['Question_Text'];
-                        String? client = e['client'];
-                        List<Widget> toReturn = [];
-                        if (bot != null) {
-                          toReturn.add(
-                            ChatBuble(
-                              text: bot ?? "",
-                              isMe: false,
-                            ),
-                          );
-                        }
-                        if (client != null) {
-                          toReturn.add(
-                            ChatBuble(
-                              text: client ?? "",
-                              isMe: true,
-                            ),
-                          );
-                        }
-                        return Column(
-                            children:
-                                toReturn.isEmpty ? [Text("error")] : toReturn);
-                      }).toList()),
-          ),
-          TextInput(returnText: returnText)
-        ],
+      body: Container(
+        padding: EdgeInsets.symmetric(vertical: 18),
+        child: Column(
+          children: [
+            Flexible(
+              child: ListView.builder(
+                padding: EdgeInsets.symmetric(vertical: 18.0),
+                itemCount: history.isEmpty ? 1 : history.length,
+                itemBuilder: (context, index) {
+                  if (history.isEmpty) {
+                    return ChatBuble(text: "No history", isMe: true);
+                  }
+
+                  String bot = history[index]['bot']['Question_Text'] ?? "";
+                  String client = history[index]['client'] ?? "";
+
+                  List<Widget> toReturn = [];
+                  if (bot.isNotEmpty) {
+                    toReturn.add(ChatBuble(text: bot, isMe: false));
+                  }
+                  if (client.isNotEmpty) {
+                    toReturn.add(ChatBuble(text: client, isMe: true));
+                  }
+
+                  return Padding(
+                    padding: EdgeInsets.symmetric(vertical: 8.0),
+                    child: Column(children: toReturn),
+                  );
+                },
+              ),
+            ),
+            TextInput(returnText: returnText)
+          ],
+        ),
       ),
     );
   }
