502ccabbc775b3bacc65b2d02ded4fdd594c551e
Marek Wannepain <marekwannepain@gmail.com>
2025-02-02 09:57:34 +0100

    Replace MainOnboarding with CareerCard widget in main app

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:adwis_frontend/pages/onboarding/main_onboarding.dart';
 import 'package:flutter/material.dart';
+import 'package:adwis_frontend/pages/home/sub/career/career_card.dart';
 
 void main() {
   runApp(MainApp());
@@ -15,7 +16,12 @@ class MainApp extends StatelessWidget {
       home: Scaffold(
         body: Container(
           padding: EdgeInsets.all(6),
-          child: MainOnboarding(),
+          // child: MainOnboarding(),
+          child: CareerCard(
+              title: "Career Card",
+              description:
+                  "Lorem ipsum dolor sit amet, consectetur.  Lorem ipsum dolor sit amet, consectetur",
+              salary: "10 000"),
         ),
       ),
     );
