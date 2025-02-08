723d72a6c6f44326997f8c87d73eb071ab3b8e17
Marek Wannepain <marekwannepain@gmail.com>
2025-02-08 16:02:06 +0100

    Integrate ApiService for user sign-up after Google authentication in AuthCardOverlay

--- lib/pages/auth/auth_card_overlay.dart
+++ lib/pages/auth/auth_card_overlay.dart
@@ -1,66 +1,8 @@
-// import 'package:flutter/material.dart';
-// import 'package:adwis_frontend/services/auth_service.dart';
-// import 'package:adwis_frontend/pages/auth/utils/sign_up_card.dart';
-// import 'package:adwis_frontend/pages/auth/utils/account_button.dart';
-
-// class AuthCardOverlay extends StatefulWidget {
-//   const AuthCardOverlay({super.key});
-
-//   @override
-//   State<AuthCardOverlay> createState() => _AuthCardOverlayState();
-// }
-
-// class _AuthCardOverlayState extends State<AuthCardOverlay> {
-//   bool isOpen = false;
-
-//   void onTap() {
-//     AuthService().signInWithGoogle();
-//   }
-
-//   void close() {
-//     setState(() {
-//       isOpen = !isOpen;
-//     });
-//   }
-
-//   @override // make the content overlay existing elements in ui
-//   Widget build(BuildContext context) {
-//     // return Column(
-//     //   mainAxisAlignment:
-//     //       isOpen ? MainAxisAlignment.end : MainAxisAlignment.start,
-//     //   crossAxisAlignment: CrossAxisAlignment.stretch,
-//     //   children: [
-//     // isOpen
-//     //     ? SignUpCard(
-//     //         close: close,
-//     //         onTap: onTap,
-//     //       )
-//     //     : AccountButton(
-//     //         close: close,
-//     //       ),
-//     //     SizedBox(
-//     //       height: 36,
-//     //     )
-//     //   ],
-//     // );
-//     return Positioned(
-//       top: 0,
-//       child: isOpen
-//           ? SignUpCard(
-//               close: close,
-//               onTap: onTap,
-//             )
-//           : AccountButton(
-//               close: close,
-//             ),
-//     );
-//   }
-// }
-
 import 'package:flutter/material.dart';
 import 'package:adwis_frontend/services/auth_service.dart';
 import 'package:adwis_frontend/pages/auth/utils/sign_up_card.dart';
 import 'package:adwis_frontend/pages/auth/utils/account_button.dart';
+import 'package:adwis_frontend/services/api_service.dart';
 
 class AuthCardOverlay extends StatefulWidget {
   const AuthCardOverlay({super.key});
@@ -71,9 +13,14 @@ class AuthCardOverlay extends StatefulWidget {
 
 class _AuthCardOverlayState extends State<AuthCardOverlay> {
   bool isOpen = false;
+  final ApiService authService = ApiService(
+    url: "https://145e-45-84-122-3.ngrok-free.app",
+  );
 
-  void onTap() {
-    AuthService().signInWithGoogle();
+  void onTap() async {
+    final userCredential = await AuthService().signInWithGoogle();
+    final result = await authService.signUp(user: userCredential.user);
+    print(result);
   }
 
   void close() {
