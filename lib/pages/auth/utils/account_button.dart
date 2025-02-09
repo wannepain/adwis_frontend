2e2befc0efee9ed76ece33fb9fe0ecf7d21b450c
Marek Wannepain <marekwannepain@gmail.com>
2025-02-09 09:55:48 +0100

    Refactor AccountButton to use Riverpod for authentication state; update UI to display user image when signed in

--- lib/pages/auth/utils/account_button.dart
+++ lib/pages/auth/utils/account_button.dart
@@ -1,11 +1,14 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:adwis_frontend/providers/auth_providers.dart';
 
-class AccountButton extends StatelessWidget {
+class AccountButton extends ConsumerWidget {
   final Function close;
   const AccountButton({super.key, required this.close});
 
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, WidgetRef ref) {
+    final data = ref.watch(authProvider);
     return Positioned(
       top: (MediaQuery.of(context).size.height / 5) * 2,
       right: 12,
@@ -23,25 +26,41 @@ class AccountButton extends StatelessWidget {
             Radius.circular(1000),
           ),
         ),
-        child: IconButton(
-          onPressed: () {
-            close();
-          },
-          icon: Icon(
-            Icons.account_circle_rounded,
-            size: 48,
-            color: Colors.black,
-          ),
-          color: Color.fromRGBO(252, 254, 255, 1),
-          style: ButtonStyle(
-            backgroundColor: WidgetStateProperty.all(
-              Color.fromRGBO(252, 254, 255, 1),
-            ),
-            padding: WidgetStateProperty.all(
-              EdgeInsets.all(0),
-            ),
-          ),
-        ),
+        child: data["isSignedIn"]
+            ? GestureDetector(
+                onTap: () {
+                  close();
+                },
+                child: ClipRRect(
+                  borderRadius: BorderRadius.circular(
+                      1000), // Ensures the image is clipped to a circle
+                  child: Image.network(
+                    data["userImage"],
+                    fit: BoxFit.cover,
+                    height: 48,
+                    width: 48,
+                  ),
+                ),
+              )
+            : IconButton(
+                onPressed: () {
+                  close();
+                },
+                icon: Icon(
+                  Icons.account_circle_rounded,
+                  size: 48,
+                  color: Colors.black,
+                ),
+                color: Color.fromRGBO(252, 254, 255, 1),
+                style: ButtonStyle(
+                  backgroundColor: WidgetStateProperty.all(
+                    Color.fromRGBO(252, 254, 255, 1),
+                  ),
+                  padding: WidgetStateProperty.all(
+                    EdgeInsets.all(0),
+                  ),
+                ),
+              ),
       ),
     );
   }
