3d012036dafdfd4be88a733b6b0b1d072568fd20
Marek Wannepain <marekwannepain@gmail.com>
2025-02-09 09:57:30 +0100

    Add AuthNotifier and authProvider for authentication state management

---
+++ lib/providers/auth_providers.dart
@@ -0,0 +1,27 @@
+import "package:flutter_riverpod/flutter_riverpod.dart";
+import "package:adwis_frontend/services/auth_service.dart";
+
+class AuthNotifier extends StateNotifier<Map> {
+  AuthNotifier()
+      : super(
+          //sets initial data
+          {
+            "isUnlimited": false,
+            "userImage": false,
+            "isSignedIn": false,
+          },
+        );
+  void checkSignedIn() async {
+    final result = await AuthService().isSignedIn();
+    state = result;
+  }
+
+  void signIn() async {
+    final result = await AuthService().signIn();
+    state = result;
+  }
+}
+
+final authProvider = StateNotifierProvider<AuthNotifier, Map>((ref) {
+  return AuthNotifier();
+});
