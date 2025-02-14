5cb665bfec53347aa0aa7a44837984951a566511
Marek Wannepain <marekwannepain@gmail.com>
2025-02-14 08:56:08 +0100

    Refactor main.dart to use const constructors and initialize Stripe publishable key

--- lib/main.dart
+++ lib/main.dart
@@ -1,15 +1,48 @@
+// import 'package:flutter/material.dart';
+// import 'package:firebase_core/firebase_core.dart';
+// import 'package:flutter_riverpod/flutter_riverpod.dart';
+// import 'firebase_options.dart';
+// import 'package:adwis_frontend/app.dart';
+// import 'package:flutter_stripe/flutter_stripe.dart';
+
+// const stripePublishableKey = "pk_test_51QpkBuGg7dfr3NgwNbbGJXQ8aJH9JrL8OIF8wXVvxE8y25soDdtnqUc26S7IqLSqeV8KCbk1OrrjHkZfxxhkikZS00PmavGBFg";
+
+// void main() async {
+//   WidgetsFlutterBinding.ensureInitialized();
+//   await Stripe.publishableKey = stripePublishableKey;
+//   await Firebase.initializeApp(
+//     options: DefaultFirebaseOptions.currentPlatform,
+//   );
+//   runApp(MainApp());
+// }
+
+// class MainApp extends StatelessWidget {
+//   const MainApp({super.key});
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return ProviderScope(child: App());
+//   }
+// }
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'firebase_options.dart';
 import 'package:adwis_frontend/app.dart';
+import 'package:flutter_stripe/flutter_stripe.dart';
+
+const stripePublishableKey =
+    "pk_test_51QpkBuGg7dfr3NgwNbbGJXQ8aJH9JrL8OIF8wXVvxE8y25soDdtnqUc26S7IqLSqeV8KCbk1OrrjHkZfxxhkikZS00PmavGBFg";
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  Stripe.publishableKey = stripePublishableKey;
+
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
   );
-  runApp(MainApp());
+
+  runApp(const MainApp());
 }
 
 class MainApp extends StatelessWidget {
@@ -17,6 +50,6 @@ class MainApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return ProviderScope(child: App());
+    return const ProviderScope(child: App());
   }
 }
