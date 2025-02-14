f366ddd4c37283571d935d39eda16f216bca3812
Marek Wannepain <marekwannepain@gmail.com>
2025-02-14 08:54:56 +0100

    Update ApiService to change API URL and add createPaymentIntent method for subscription handling

--- lib/services/api_service.dart
+++ lib/services/api_service.dart
@@ -1,40 +1,11 @@
 import 'package:dio/dio.dart';
-import 'package:cloud_firestore/cloud_firestore.dart';
 
 class ApiService {
-  final String url = "https://edf9-45-84-122-5.ngrok-free.app";
+  final String url = "https://adwis-api.onrender.com";
 
   ApiService();
 
   final dio = Dio();
-
-  Future<Map> chatbotRespond(
-      {required List history, required List usedQuestionIdx}) async {
-    try {
-      final response = await dio.post("$url/respond",
-          data: {"history": history, "used_question_idx": usedQuestionIdx});
-
-      return {
-        "history": response.data['history'],
-        "usedQuestionIdx": response.data['used_question_idx'],
-        "error": false,
-      };
-    } catch (e) {
-      print('Error: $e');
-      return {"history": null, "usedQuestionIdx": null, "error": e};
-    }
-  }
-
-  Future<Map> getCareer({required history}) async {
-    try {
-      final response =
-          await dio.post("$url/career", data: {"history": history});
-      return response.data["career"];
-    } catch (e) {
-      print('Error: $e');
-      return {"history": null, "usedQuestionIdx": null, "error": e};
-    }
-  }
 
   Future<Map> signUp({required user}) async {
     try {
@@ -56,5 +27,14 @@ class ApiService {
     } catch (e) {
       return {"error": e, "success": false};
     }
+  }
+
+  Future<String> createPaymentIntent(String uid) async {
+    final response = await dio.post(
+      '$url/api/create-subscription',
+      data: {'uid': uid},
+    );
+    final responseData = response.data;
+    return responseData["clientSecret"];
   }
 }
